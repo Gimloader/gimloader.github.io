acc1940e6582a543b3eb94519ce8211754e7b865
TheLazySquid <thelazysquid2@gmail.com>
2026-02-23 09:26:56 -0500

    Infer downloadUrl based on name

--- src/lib/scripts.ts
+++ src/lib/scripts.ts
@@ -13,19 +13,19 @@ interface Config {
 
 const descriptionRegex = /description:\s*"([^"]+)"/;
 const authorRegex = /author:\s*"([^"]+)"/;
-const downloadUrlRegex = /downloadUrl:\s*"([^"]+)"/;
 const reloadRequiredRegex = /reloadRequired:\s*(true|false|"[^"]+")/;
 const needsPluginsRegex = /needsPlugins:\s*\[([^\]]+)\]/;
+
+const baseDownloadUrl = "https://raw.githubusercontent.com/Gimloader/client-plugins/main/build";
 
 export async function readConfig(category: ScriptCategory, name: string): Promise<Config> {
     const content = await readFile(`./client-plugins/${category}/${name}/gimloader.config.ts`, "utf-8");
     
     const descriptionMatch = content.match(descriptionRegex);
     const authorMatch = content.match(authorRegex);
-    const downloadUrlMatch = content.match(downloadUrlRegex);
     const reloadRequiredMatch = content.match(reloadRequiredRegex);
     const needsPluginsMatch = content.match(needsPluginsRegex);
-    if(!descriptionMatch || !authorMatch || !downloadUrlMatch) throw new Error(`Invalid config for ${category} ${name}`);
+    if(!descriptionMatch || !authorMatch) throw new Error(`Invalid config for ${category} ${name}`);
 
     // Convert reloadRequired to a boolean or string
     let reloadRequired: string | boolean = false;
@@ -43,7 +43,7 @@ export async function readConfig(category: ScriptCategory, name: string): Promis
         name,
         description: descriptionMatch[1],
         author: authorMatch[1],
-        downloadUrl: downloadUrlMatch[1],
+        downloadUrl: `${baseDownloadUrl}/${category}/${name}.js`,
         reloadRequired,
         desyncs
     };
